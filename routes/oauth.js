0dbc75a7351d5807591a3eee4689c54d8239cf20
tao24 <tao24.k@gmail.com>
2018-05-19 20:48:36 +0200

    UJHGDF

--- routes/oauth.js
+++ routes/oauth.js
@@ -4,11 +4,10 @@ var router = express.Router();
 /* GET oauth. */
 router.get('/', function(req, res) {
     console.log('GET REQUEST');
-    console.log(req.body);
+    console.log(req);
     res.sendStatus(200);
     //res.send('fblogin', {appId : process.env.APPLICATION_ID});
 });
-
 
 router.post('/', function(req, res) {
     console.log("POST REQUEST");
