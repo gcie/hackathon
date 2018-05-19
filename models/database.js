02049ec596ebfae874fbb37dfa5efafb51ed5cc4
tao24 <tao24.k@gmail.com>
2018-05-19 17:09:02 +0200

    2.6 -> 3.0

---
+++ models/database.js
@@ -0,0 +1,29 @@
+var pgp = require('pg-promise')();
+
+var Database = function() {
+    this.db = pgp(process.env.DATABASE_URL);
+    return this;
+};
+
+Database.prototype.checkUser = function(user) {
+    return new Promise(function(res, rej) {
+        this.db.oneOrNone('SELECT id FROM public.users WHERE email=${email}', user)
+        .then(function(id) {
+            res(id !== null);
+        }, rej);
+    });
+};
+
+// user: { psid }
+Database.prototype.insertUser = function(user) {
+    user.username = user.username || null;
+    return this.db.none("INSERT INTO users(psid) VALUES (${psid});", user);
+};
+
+Database.prototype.deleteUser = function(user) {
+    return this.db.none("DELETE FROM public.users WHERE psid=${psid}", user);
+};
+
+Database.prototype.getUser
+
+Database.prototype.isUserTaken = function(user) {}
--- routes/webhook.js
+++ routes/webhook.js
@@ -41,7 +41,7 @@ function sendTextMessage(sender, text) {
 	let messageData = { text: text };
 	
 	request({
-		url: 'https://graph.facebook.com/v2.6/me/messages',
+		url: 'https://graph.facebook.com/v3.0/me/messages',
 		qs: { access_token: process.env.PAGE_MSG_TOKEN },
 		method: 'POST',
 		json: {
