a3092130909ed16c9ebd81f8ba7156dd75553956
tao24 <tao24.k@gmail.com>
2018-05-19 18:37:51 +0200

    OAuth

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET oauth. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('fblogin', {appId : process.env.APPLICATION_ID});
 });
 
 
