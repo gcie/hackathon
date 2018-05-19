dba0d179bb17fecc7966e84251ca300e6cc9ff88
tao24 <tao24.k@gmail.com>
2018-05-20 01:02:53 +0200

    Db rework

--- models/database.js
+++ models/database.js
@@ -1,20 +1,73 @@
 var pgp = require('pg-promise')();
 
-var Database = function() {
-    var cn = process.env.DATABASE_URL;
-    if(cn == undefined) {
-        cn = {
-            host: 'localhost',
-            port: 5432,
-            database: 'postgres',
-            user: 'gucci',
-            password: 'gucci'
-        };
-    }
-    this.db = pgp(cn);
-    return this;
-};
+var Database = (function() {
+    var db;
 
+    function init() {
+        var cn = process.env.DATABASE_URL;
+        if(cn == undefined) {
+            cn = {
+                host: 'localhost',
+                port: 5432,
+                database: 'postgres',
+                user: 'gucci',
+                password: 'gucci'
+            };
+        }
+        return pgp(cn);
+    }
+
+    var get = function() {
+        if(!db) {
+            db = init();
+        }
+        return db;
+    }
+
+    return {
+        get: get,
+        checkUser: function(psid) {
+            get();
+            return new Promise(function(res, rej) {
+                db.oneOrNone('SELECT token FROM users WHERE psid=$1', psid)
+                .then(function(token) {
+                    res(token !== null);
+                }, rej);
+            });
+        },
+        getUserMatch: function(psid) {
+            get();
+            return db.oneOrNone("SELECT int_psid FROM users WHERE psid=$1", psid);
+        },
+        getUserToken: function(psid) {
+            get();
+            return db.oneOrNone("SELECT token FROM users WHERE psid=$1", psid);
+        },
+        getUsers: function() {
+            get();
+            return db.any("SELECT psid, token, int_psid FROM users");
+        },
+        insertUser: function(user) {
+            get();
+            return db.oneOrNone('SELECT token FROM users WHERE psid=${psid}', user)
+            .then(token => {
+                if(token === null) {
+                    return db.none("INSERT INTO users(psid, token) VALUES (${psid}, ${token});", user);
+                } else {
+                    return db.none("UPDATE users SET token=${token} WHERE psid=${psid};", user);
+                }
+            });
+        },
+        deleteUser: function(user) {
+            get();
+            return db.none("DELETE FROM public.users WHERE psid=${psid}", user);
+        }
+    }
+})();
+
+
+
+/*
 Database.prototype.checkUser = function(psid) {
     return new Promise(function(res, rej) {
         this.db.oneOrNone('SELECT token FROM users WHERE psid=$1', psid)
@@ -50,5 +103,6 @@ Database.prototype.insertUser = function(user) {
 Database.prototype.deleteUser = function(user) {
     return this.db.none("DELETE FROM public.users WHERE psid=${psid}", user);
 };
+*/
 
 module.exports = Database;
