1120286ee42913fd093e389886f994060e55a36a
tao24 <tao24.k@gmail.com>
2018-05-20 08:13:40 +0200

    Greeting message

--- routes/oauth.js
+++ routes/oauth.js
@@ -9,6 +9,8 @@ router.get('/', function(req, res) {
     var user = User(req.query.psid, req.query.token);
     db.insertUser(user);
 
+    sendMatchQuestion(req.query.psid, "Login successful! ", "Would you like to find your first match?");
+
     res.redirect(307, 'https://www.messenger.com/closeWindow/?image_url=&display_text=Login%20successful%2C%20you%20may%20close%20this%20page.');
 });
 
@@ -19,3 +21,45 @@ router.post('/', function(req, res) {
 });
 
 module.exports = router;
+
+function sendMatchQuestion(sender, text, subtext){	
+	request({
+		url: 'https://graph.facebook.com/v3.0/me/messages',
+		qs: { access_token: process.env.PAGE_MSG_TOKEN },
+		method: 'POST',
+		json: {
+			messaging_type : 'RESPONSE',
+			"recipient":{
+			  	"id": sender
+			},
+			"message":{
+				"attachment":{
+					"type":"template",
+					"payload":{
+						"template_type":"generic",
+						"elements":[
+							{
+								"title": text,
+								"subtitle": subtext,
+								
+								"buttons":[ {
+										"title": "Find a match",
+										"type": "postback",
+										"payload": "FIND_MATCH"
+									}]      
+							}
+						]
+					}
+				}
+			}
+		}
+		  
+	}, function(error, response, body) {
+		if (error) {
+			console.log('Error sending messages: ', error);
+		} else if (response.body.error) {
+			console.log('Error: ', response.body.error);
+		}
+		console.log(body);
+	});
+}
