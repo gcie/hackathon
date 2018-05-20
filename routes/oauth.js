6469dc588dd38643b1b259de59010b5d3f10897e
tao24 <tao24.k@gmail.com>
2018-05-20 08:17:29 +0200

    Jebany request v.2

--- routes/oauth.js
+++ routes/oauth.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 var db = require('../models/database.js');
 var User = require('../models/user.js');
 
